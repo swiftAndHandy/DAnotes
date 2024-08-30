06d5deabab0011c4e69a5e9ac107aa18f5158f70
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-08-30 15:08:18 +0200

    real access to firebase added

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,77 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { collectionData, Firestore, collection, doc, onSnapshot } from '@angular/fire/firestore';
+import { Note } from '../interfaces/note.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
-  constructor() { }
+  trashNotes: Note[] = [];
+  regularNotes: Note[] = [];
+
+  unsubTrash;
+  unsubNotes;
+
+  items$;
+
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+
+    this.unsubNotes = this.subNotesList();
+    this.unsubTrash = this.subTrashList();
+
+    this.items$ = collectionData(this.getNotesRef());
+    
+
+  }
+
+  ngOnDestroy() {
+    this.unsubNotes();
+    this.unsubTrash();
+  }
+
+
+  setNotesObject(obj: any, id: string): Note {
+    return {
+      'id': id || '',
+      'title': obj.title || '',
+      'content': obj.content || '',
+      'type': obj.type || 'note',
+      'marked': obj.marked || false,
+    }
+  }
+
+  subTrashList() {
+    return onSnapshot(this.getTrashRef(), (list) => {
+      //set to an empty array first? i doubt it.
+      list.forEach(element => {
+        this.trashNotes.push(this.setNotesObject(element.data(), element.id));
+      })
+    });
+  }
+
+  subNotesList() {
+    return onSnapshot(this.getNotesRef(), (list) => {
+      //set to an empty array first? i doubt it.
+      list.forEach(element => {
+        this.regularNotes.push(this.setNotesObject(element.data(), element.id));
+      })
+    });
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  // getSingleDocRef(collectionId: string, docId: string) {
+  //   return doc(collection(this.firestore, collectionId), docId)
+  // }
+
 }
--- src/app/note-list/note-list.component.ts
+++ src/app/note-list/note-list.component.ts
@@ -19,63 +19,30 @@ export class NoteListComponent {
   favFilter: "all" | "fav" = "all";
   status: "notes" | "trash" = "notes";
 
-  constructor(public noteService: NoteListService) {
-    this.noteList = this.getDummyData()
+  constructor(private noteService: NoteListService) {
+
   }
 
-  changeFavFilter(filter:"all" | "fav"){
+  getList(typeOfNotes: 'regular' | 'trashed' ): Note[] {
+    switch (typeOfNotes) {
+      case 'regular':
+        return this.noteService.regularNotes;
+      case 'trashed':
+        return this.noteService.trashNotes;
+    }
+  }
+
+  changeFavFilter(filter: "all" | "fav") {
     this.favFilter = filter;
   }
 
-  changeTrashStatus(){
-    if(this.status == "trash"){
+  changeTrashStatus() {
+    if (this.status == "trash") {
       this.status = "notes";
     } else {
       this.status = "trash";
       this.favFilter = "all";
     }
-  }
-
-
-
-
-  getDummyData(): Note[] {
-    return [
-      {
-        id: "21sasd561dd4sdf",
-        type: "note",
-        title: "Block, Inline, and Inline-Block",
-        content: "https://www.youtube.com/watch?v=x_i2gga-sYg",
-        marked: true,
-      },
-      {
-        id: "25sd4f561w54sdf",
-        type: "note",
-        title: "css selector",
-        content: `kind p > b   (direktes kind) 
-        nachfahren p b  (alle nachfahren)
-        geschwister p ~ b (auf gleicher ebene ist VOR dem p ein b)`,
-        marked: true,
-      },
-      {
-        id: "54a4s6d546ff",
-        type: "note",
-        title: "aufräumen",
-        content: "Wohnzimmer saugen",
-        marked: false,
-      },
-      {
-        id: "2a35s4d654a6s4d",
-        type: "note",
-        title: "links",
-        content: `Reihenfolge: a:visited 
-        a:focus 
-        a:hover 
-        a:active
-        merkspruch: LoVe HAte`,
-        marked: true,
-      }
-    ];
   }
 
 }
